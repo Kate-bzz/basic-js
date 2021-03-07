ad0f24f3dc24c83106dc7412bc48034630677116
Kate-bzz <73671083+Kate-bzz@users.noreply.github.com>
2021-03-07 16:26:56 +0300

    fix: add try catch

--- src/what-season.js
+++ src/what-season.js
@@ -1,27 +1,33 @@
-const Seasons = {
-  1: 'winter',
-  2: 'winter',
-  3: 'spring',
-  4: 'spring',
-  5: 'spring',
-  6: 'summer',
-  7: 'summer',
-  8: 'summer',
-  9: 'autumn',
-  10: 'autumn',
-  11: 'autumn',
-  12: 'winter'
-};
 const CustomError = require("../extensions/custom-error");
+
 module.exports = function getSeason(date) {
-  try{let result='';
-  if(date===undefined)
+  if (!date) { 
     return 'Unable to determine the time of year!';
-  for (let key in Seasons) {
-    if(key === month(date)) result.push(`${Seasons[key]}`);
-}
-return result;}
-catch(e){
-    throw new CustomError('Not implemented');
-}
-}
+  }
+
+  try {
+    date.getTime();
+  } catch(err) {
+    throw new Error(err);
+  }
+
+  const month = date.getMonth() + 1;
+  switch (month) {
+    case 1:
+    case 2:
+    case 12:
+      return 'winter';
+    case 3:
+    case 4:
+    case 5:
+      return 'spring';
+    case 6:
+    case 7:
+    case 8:
+      return 'summer';
+    case 9:
+    case 10:
+    case 11:
+       return 'autumn';
+  }
+};
